e0e66a5326172527765dff1da29b01b10c88ac42
Brenden Haskins <brendenshaskins@gmail.com>
2024-12-01 22:18:53 -0800

    day 2 part 2

--- Solutions/DayTwo.cs
+++ Solutions/DayTwo.cs
@@ -5,6 +5,7 @@ namespace Solutions;
 class DayTwo {
     public static void Run() {
         int safeCount = 0;
+        int dampenerSafeCount = 0;
 
         using (StreamReader fileInput = new StreamReader("input2.txt")) {
             string line;
@@ -17,10 +18,14 @@ class DayTwo {
                 if(LevelIsSafe(list)) {
                     safeCount++;
                 }
+                if(LevelIsSafeWithDampener(list)) {
+                    dampenerSafeCount++;
+                }
             }
         }
 
         Console.WriteLine(safeCount);
+        Console.WriteLine(dampenerSafeCount);
 
     }
 
@@ -49,5 +54,46 @@ class DayTwo {
         }
 
         return true;
+    }
+
+    static bool LevelIsSafeWithDampener(List<int> level) {
+        int isIncreasing;
+        if(level[0] < level[1]) {
+            isIncreasing = 3;
+        } else if(level[0] > level[1]) {
+            isIncreasing = -3;
+        } else {
+            return DampenerInspection(level);
+        }
+
+        for(int i = 0; i < level.Count-1; i++) {
+            int diff = level[i+1] - level[i];
+
+            //if the difference is greater than three, it increased/decreased too much
+            if(Math.Abs(diff) > 3) {
+                return DampenerInspection(level);
+            }
+            
+            //if the difference did not bring isIncreasing further away from zero, it failed to increase/decrease
+            if(!(Math.Abs(diff+isIncreasing) > Math.Abs(isIncreasing))) {
+                return DampenerInspection(level);
+            }
+        }
+
+        return true;
+    }
+
+    static bool DampenerInspection(List<int> level) {
+        //this actually exponentiates our work, but I can't figure out the faster way to do it
+        var haystack = new List<bool>();
+
+        for(int i = 0; i < level.Count; i++) {
+            List<int> duplicate = level.ToList();
+            duplicate.RemoveAt(i);
+            haystack.Add(LevelIsSafe(duplicate));
+        }
+
+
+        return haystack.Contains(true);
     }
 }
